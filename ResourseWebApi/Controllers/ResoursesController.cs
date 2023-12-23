33c0b95926968381383b0a69075f5ed5582da1f0
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2023-12-23 16:23:08 +0300

    add some routes

--- ResourseWebApi/Controllers/ResoursesController.cs
+++ ResourseWebApi/Controllers/ResoursesController.cs
@@ -1,6 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
+using ResourseWebApi.Contracts.Resourse;
 
 namespace ResourseWebApi.Controllers;
 
 [ApiController]
-public class ResoursesController : ControllerBase { }
+public class ResoursesController : ControllerBase
+{
+    [HttpPost("/resourse")]
+    public IActionResult CreateResourse(CreateResourseRequest request)
+    {
+        return Ok(request);
+    }
+
+    [HttpGet("/resourse/{id:guid}")]
+    public IActionResult GetResourse(Guid id)
+    {
+        return Ok(id);
+    }
+
+    [HttpPut("/resourse/{id:guid}")]
+    public IActionResult UpsertResourse(Guid id, UpsertResourseRequest request)
+    {
+        return Ok(request);
+    }
+
+    [HttpDelete("/resourse/{id:guid}")]
+    public IActionResult DeleteResourse(Guid id)
+    {
+        return Ok(id);
+    }
+}
